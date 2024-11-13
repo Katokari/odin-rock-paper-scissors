397a3f38ea58e48f37b692d0378f092216b711ec
Katokari <katokarix@gmail.com>
2024-11-13 16:24:15 +0300

    Create function to get choice from the player

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ const Choice = ["rock", "paper", "scissors"];
 
 let getComputerChoice = () => Choice[Math.floor(Math.random() * 3)];
 
+let getHumanChoice = () => Choice[parseInt(prompt("Please enter a number of the following:\n[1] : rock\n[2] : paper\n[3] : scissors")) - 1];
+
