47195d60c8c36478df553d3306d620437bdba36f
Katokari <katokarix@gmail.com>
2024-11-16 00:57:43 +0300

    Add gui to the game with basic event handling

--- script.js
+++ script.js
@@ -1,44 +1,50 @@
 const Choice = ["rock", "paper", "scissors"];
+let humanScore = 0,
+    computerScore = 0;
+
+const playerChoiceContainer = document.querySelector(".player-choice");
+const resultsPanel = document.querySelector("#round-result");
+const humanScorePanel = document.querySelector("#humanScore");
+const computerScorePanel = document.querySelector("#computerScore");
+const buttons = document.querySelectorAll("button");
 
 const getComputerChoice = () => Choice[Math.floor(Math.random() * 3)];
-
-const getHumanChoice = () => Choice[parseInt(prompt("Please enter a number of the following:\n[1] : rock\n[2] : paper\n[3] : scissors")) - 1];
 
 const getRoundWinner = (computerChoice, humanChoice) => {
     switch (computerChoice) {
         case "rock":
             if (humanChoice == "rock") {
-                console.log("You chose rock and computer chose rock, it's a tie!");
+                resultsPanel.textContent = "You chose rock and computer chose rock, it's a tie!";
                 return 2;
             } else if (humanChoice == "paper") {
-                console.log("You chose paper and computer chose rock, you win!");
+                resultsPanel.textContent = "You chose paper and computer chose rock, you win!";
                 return 0;
             } else {
-                console.log("You chose scissors and computer chose rock, you lost!");
+                resultsPanel.textContent = "You chose scissors and computer chose rock, you lost!";
                 return 1;
             }
 
         case "paper":
             if (humanChoice == "rock") {
-                console.log("You chose rock and computer chose paper, you lost!");
+                resultsPanel.textContent = "You chose rock and computer chose paper, you lost!";
                 return 1;
             } else if (humanChoice == "paper") {
-                console.log("You chose paper and computer chose paper, it's a tie!");
+                resultsPanel.textContent = "You chose paper and computer chose paper, it's a tie!";
                 return 2;
             } else {
-                console.log("You chose scissors and computer chose paper, you win!");
+                resultsPanel.textContent = "You chose scissors and computer chose paper, you win!";
                 return 0;
             }
 
         case "scissors":
             if (humanChoice == "rock") {
-                console.log("You chose rock and computer chose scissors, you win!");
+                resultsPanel.textContent = "You chose rock and computer chose scissors, you win!";
                 return 0;
             } else if (humanChoice == "paper") {
-                console.log("You chose paper and computer chose scissors, you lost!");
+                resultsPanel.textContent = "You chose paper and computer chose scissors, you lost!";
                 return 1;
             } else {
-                console.log("You chose scissors and computer chose scissors, it's a tie!");
+                resultsPanel.textContent = "You chose scissors and computer chose scissors, it's a tie!";
                 return 2;
             }
     }
@@ -46,29 +52,55 @@ const getRoundWinner = (computerChoice, humanChoice) => {
 
 const getHowManyRounds = () => parseInt(prompt("How many round do you want to play?"));
 
-function playGame () {
-    let humanScore = 0, 
-        computerScore = 0;
-        tieScore = 0;
+const endGame = () => {
+    playerChoiceContainer.removeEventListener('click', playerChoiceHandler);
 
-    const playRound = () => {
-        let computerChoice = getComputerChoice(),
-            humanChoice = getHumanChoice();
-    
-        let roundWinner = getRoundWinner(computerChoice, humanChoice);
-    
-        roundWinner === 0 ? humanScore++ : roundWinner === 1 ? computerScore++ : tieScore++;
-    }  
-    
-    const roundsCount = getHowManyRounds();
-    
-    for (let i = 0; i < roundsCount; i++) {
-        playRound();
-    }
-
-    const endMessage = "You won " + humanScore + " times, computer won " + computerScore + " times, with " + tieScore + " time tie, overall ";
-
-    humanScore > computerScore ? console.log(endMessage + " you won!") : computerScore > humanScore ? console.log(endMessage + " you lost!") : console.log(endMessage + " its a tie!");
+    buttons.forEach(btn => {
+        btn.classList.remove("hovered");
+        btn.style.cssText = "background-color: grey;";
+    });
 }
 
-playGame();
+const editScores = (winner) => {
+    if (winner === 0) humanScore++;
+    else if (winner === 1) computerScore++;
+
+    humanScorePanel.textContent = "Human Score : " + humanScore;
+    computerScorePanel.textContent = "Computer Score : " + computerScore;
+
+    if (computerScore === 5) {
+        endGame();
+        resultsPanel.textContent = "Computer Won!";
+    } else if (humanScore === 5) {
+        endGame();
+        resultsPanel.textContent = "You Won!";
+    }
+}
+
+const playRound = (humanChoice) => {
+    let computerChoice = getComputerChoice();
+        
+    humanChoice = Choice[humanChoice];
+
+    let roundWinner = getRoundWinner(computerChoice, humanChoice);
+
+    editScores(roundWinner);
+}
+
+const playerChoiceHandler = (e) => {
+    let target = e.target;
+
+    switch (target.id) {
+        case 'rock':
+            playRound(0);
+            break;
+        case 'paper':
+            playRound(1);
+            break;
+        case 'scissors':
+            playRound(2);
+            break;
+    }
+};
+
+playerChoiceContainer.addEventListener('click', playerChoiceHandler);
